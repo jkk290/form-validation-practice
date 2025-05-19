11f18343a272f544f9e2202c55f5c37bab60fe2b
jkk290 <158115736+jkk290@users.noreply.github.com>
2025-05-19 13:17:03 -1000

    Add form validation for email address input

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 import "./styles.css";
 
-alert("Hello World");
+const form = document.querySelector('form');
+const email = document.getElementById('email');
+const emailError = document.querySelector('#email + span.error');
+
+email.addEventListener('input', (event) => {
+    if (email.validity.valid) {
+        emailError.textContent = '';
+        emailError.className = 'error';
+    } else {
+        showError();
+    }
+});
+
+
+form.addEventListener('submit', (event) => {
+    if (!email.validity.valid) {
+        showError();
+        event.preventDefault();
+    }
+});
+
+function showError() {
+    if (email.validity.valueMissing) {
+        emailError.textContent = 'You need to enter an email address.';
+    } else if (email.validity.typeMismatch) {
+        emailError.textContent = 'Entered value needs to be an email address.';
+    }
+
+    emailError.className = 'error active';
+
+}
